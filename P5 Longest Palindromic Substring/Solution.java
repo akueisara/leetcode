2aae108ae7f3184fe5c3d2b4782029ce0c78f0b8
Kuei <akueisara39@gmail.com>
2019-08-26 03:09:23 +0800

    Add P5

---
+++ P5 Longest Palindromic Substring/Solution.java
@@ -0,0 +1,74 @@
+class Solution {
+    public  String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) return "";
+        int n = s.length(), start = 0;
+        int maxLength = 1;
+        for (int center = 0; center <= 2*n-1; center++) {
+            int left = center / 2;
+            int right = left + center % 2;
+            while (left >= 0 && right < n && s.charAt(left) == s.charAt(right)) {
+                if (right - left + 1 > maxLength) {
+                    start = left;
+                    maxLength = right - left + 1;
+                }
+                left--;
+                right++;
+            }
+        }
+        return s.substring(start, start+maxLength);
+    }
+
+    public String longestPalindrome2(String s) {
+        // Dynamic Programming
+        if (s == null || s.length() < 1) return "";
+
+        int n = s.length();
+        // table[i][j] will be false if substring str[i..j]
+        // is not palindrome.
+        // Else table[i][j] will be true
+        boolean table[][] = new boolean[n][n];
+
+        // All substrings of length 1 are palindromes
+        int maxLength = 1;
+        for (int i = 0; i < n; ++i)
+            table[i][i] = true;
+
+        // check for sub-string of length 2.
+        int start = 0;
+        for (int i = 0; i < n - 1; ++i) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                table[i][i + 1] = true;
+                start = i;
+                maxLength = 2;
+            }
+        }
+
+        // Check for lengths greater than 2. k is length
+        // of substring
+        for (int k = 3; k <= n; ++k) {
+
+            // Fix the starting index
+            for (int i = 0; i < n - k + 1; ++i)
+            {
+                // Get the ending index of substring from
+                // starting index i and length k
+                int j = i + k - 1;
+
+                // checking for sub-string from ith index to
+                // jth index iff s.charAt(i+1) to
+                // s.charAt(j-1) is a palindrome
+                if (table[i + 1][j - 1] && s.charAt(i) ==
+                                          s.charAt(j)) {
+                    table[i][j] = true;
+
+                    if (k > maxLength) {
+                        start = i;
+                        maxLength = k;
+                    }
+                }
+            }
+        }
+
+        return s.substring(start, start+maxLength);
+    }
+}
