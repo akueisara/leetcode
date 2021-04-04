c0da4b2389ca6871b4d719fbe68adebe1a7ffe1b
Kuei-Jung Hu <akueisara39@gmail.com>
2021-04-04 20:46:41 +0900

    P127 Word Ladder

--- P127 Word Ladder/Solution.java
+++ P127 Word Ladder/Solution.java
@@ -1,60 +1,86 @@
-class Solution:
-    # Bi-BFS
-    # M is the length of each word and N is the length of the word list
-    # Time: O(M^2*N)
-    # Space: O(M*N)
-    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        words = set(wordList)
-        if endWord not in words:
-            return 0
-        begin_set = set([beginWord])
-        end_set = set([endWord])
-        length = 1
-        while len(begin_set) != 0 and len(end_set) != 0:
-            if len(begin_set) > len(end_set):
-                begin_set, end_set = end_set, begin_set
-            new_begin_set = set()
-            for word in begin_set:
-                neighbors = self._get_neighbors(word)
-                for neighbor in neighbors:
-                    if neighbor in end_set:
-                        return length + 1
-                    if neighbor in words:
-                        new_begin_set.add(neighbor)
-                        words.remove(neighbor)
-            begin_set = new_begin_set
-            length += 1
-        return 0
+class Solution {
+    // Bi-BFS
+    // M is the length of each word and N is the length of the word list
+    // Time: O(M^2*N)
+    // Space: O(M*N)
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> words = new HashSet<>(wordList);
+        if(!words.contains(endWord)) {
+            return 0;
+        }
+        Set<String> beginSet = new HashSet<>();
+        beginSet.add(beginWord);
+        Set<String> endSet = new HashSet<>();
+        endSet.add(endWord);
+        int length = 1;
+        while(!beginSet.isEmpty() && !endSet.isEmpty() ) {
+            if(beginSet.size() > endSet.size()) {
+                Set<String> temp = beginSet;
+                beginSet = endSet;
+                endSet = temp;
+            }
+            Set<String> newBeginSet = new HashSet<>();
+            for(String word: beginSet) {
+                List<String> neighbors = getNeighbors(word);
+                for(String neighbor: neighbors) {
+                    if(endSet.contains(neighbor)) {
+                        return length + 1;
+                    }
+                    if(words.contains(neighbor)) {
+                        newBeginSet.add(neighbor);
+                        words.remove(neighbor);
+                    }
+                }
+            }
+            beginSet = newBeginSet;
+            length++;
+        }
+        return 0;
+    }
     
-    # BFS
-    # M is the length of each word and N is the length of the word list
-    # Time: O(M^2*N)
-    # Space: O(M^2*N)
-    def ladderLength2(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        queue = [beginWord]
-        words = set(wordList)
-        if beginWord in words:
-            words.remove(beginWord)
-        step = 0
-        while len(queue) != 0:
-            size = len(queue)
-            step += 1
-            for i in range(size):
-                current_word = queue.pop(0)
-                if current_word == endWord:
-                    return step
-                neighbors = self._get_neighbors(current_word)
-                for neighbor in neighbors:
-                    if neighbor in words:
-                        words.remove(neighbor)
-                        queue.append(neighbor)
-                        
-        return 0
+    // BFS
+    // M is the length of each word and N is the length of the word list
+    // Time: O(M^2*N)
+    // Space: O(M^2*N)
+    public int ladderLength2(String beginWord, String endWord, List<String> wordList) {
+        Queue<String> queue = new LinkedList<>();
+        Set<String> words = new HashSet<>(wordList);
+        words.remove(beginWord);
+        queue.add(beginWord);
+        int step = 0;
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            step++;
+            for(int i = 0; i < size; i++) {
+                String currentWord = queue.poll();
+                if(currentWord.equals(endWord)) {
+                    return step;
+                }
+                List<String> neighbors = getNeighbors(currentWord); 
+                for(String neighbor: neighbors) {
+                    if(words.contains(neighbor)) {
+                        words.remove(neighbor);
+                        queue.add(neighbor);
+                    }
+                }
+            }
+        }
+        return 0;
+    }
     
-    # Time: O(M^2)
-    def _get_neighbors(self, word: str) -> List[str]:
-        result = []
-        for i in range(len(word)):
-            for n in range(ord('a'), ord('z') + 1):
-                result.append(word[:i] + chr(n) + word[i+1:])
-        return result
+    // Time: O(M^2)
+    private List<String> getNeighbors(String word) {
+        char[] chars = word.toCharArray();
+        List<String> result = new ArrayList();
+        for(int i = 0; i < chars.length; i++) {
+            char temp = chars[i];
+            for(char c = 'a'; c <= 'z'; c++) {
+                chars[i] = c;
+                String neighbor = new String(chars);
+                result.add(neighbor);
+            }
+            chars[i] = temp;
+        }
+        return result;
+    }
+}
