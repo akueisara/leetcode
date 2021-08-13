50add2cb6c21c9768088fe8f34dcfdca8b2a5153
Kuei-Jung Hu <akueisara39@gmail.com>
2021-08-13 19:40:42 +0900

    Update P56 Merge Intervals

--- P56 Merge Intervals/Solution.java
+++ P56 Merge Intervals/Solution.java
@@ -1,16 +1,19 @@
 class Solution {
+    // Time: o(nlogn)
+    // Space: O(n)
     public int[][] merge(int[][] intervals) {
-        if (intervals.length <= 1)
-			return intervals;
+        if (intervals.length <= 1) {
+            return intervals;
+        }
         Arrays.sort(intervals, (i1, i2) -> Integer.compare(i1[0], i2[0]));
         List<int[]> result = new ArrayList<>();
 		int[] newInterval = intervals[0];
 		result.add(newInterval);
-        for (int i = 1; i < intervals.length; i++) {
-            if (newInterval[1] >= intervals[i][0]) {
-				newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+        for (int[] interval: intervals) {
+            if (newInterval[1] >= interval[0]) {
+				newInterval[1] = Math.max(newInterval[1], interval[1]);
 		    } else {                           
-				newInterval = intervals[i];
+				newInterval = interval;
                 result.add(newInterval);
 			} 
         }
--- P56 Merge Intervals/solution.py
+++ P56 Merge Intervals/solution.py
@@ -1,14 +1,16 @@
 class Solution:
+    # Time: o(nlogn)
+    # Space: O(n)
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         if len(intervals) <= 1:
             return intervals
         intervals = sorted(intervals,key=lambda l:l[0])
         newInterval = intervals[0]
         result = [newInterval]
-        for i in range(1, len(intervals)):
-            if newInterval[1] >= intervals[i][0]:
-                newInterval[1] = max(newInterval[1], intervals[i][1])
+        for interval in intervals:
+            if newInterval[1] >= interval[0]:
+                newInterval[1] = max(newInterval[1], interval[1])
             else:
-                newInterval = intervals[i]
+                newInterval = interval
                 result.append(newInterval)
         return result
